ad39c9b72e4a3c437450726150dfbf6b67a63fda
Alexander Akovantcev <quidjiboo2015@gmail.com>
2016-06-29 10:59:29 +0300

    логинится. Сделать кнопку разлогинивания!

--- app/src/main/java/ru/akov/buddyinshot/MainActivity.java
+++ app/src/main/java/ru/akov/buddyinshot/MainActivity.java
@@ -2,22 +2,38 @@ package ru.akov.buddyinshot;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.MainThread;
+import android.support.annotation.StringRes;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 
+import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.ui.email.SignInActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String UNCHANGED_CONFIG_VALUE = "CHANGE-ME";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        if (auth.getCurrentUser() != null) {
+            showSnackbar(auth.getCurrentUser().toString());
+            // already signed in
+        } else {
+            // not signed in
+        }
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -32,8 +48,24 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        if (!isGoogleConfigured()) {
+            showSnackbar("Configuration is required _goo - see README.md");
+        }
+        else{ Snackbar.make(findViewById(android.R.id.content), R.string.default_web_client_id, Snackbar.LENGTH_LONG).show();
+            if(auth.getCurrentUser() == null){
+            startActivityForResult(
+                    AuthUI.getInstance().createSignInIntentBuilder()
+                            .setTheme(AuthUI.getDefaultTheme())
+                            .setProviders(AuthUI.GOOGLE_PROVIDER)
+                            .setTosUrl("https://www.google.com/policies/terms/")
+                            .build(),
+                    100);}
+        }
+
 
     }
+
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -63,5 +95,15 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
 
 
+    }
+
+    @MainThread
+    private boolean isGoogleConfigured() {
+        return !UNCHANGED_CONFIG_VALUE.equals(
+                getResources().getString(R.string.default_web_client_id));
+    }
+    @MainThread
+    private void showSnackbar(String errorMessage) {
+        Snackbar.make(findViewById(android.R.id.content), errorMessage, Snackbar.LENGTH_LONG).show();
     }
 }
